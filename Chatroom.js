267bb30f68cb90601a03896a192d725ac1dd3fb6
kguinto <klogold1@gmail.com>
2019-09-07 16:17:59 -0700

    Refactor to use class pattern for chatroom (user map and message list)

---
+++ Chatroom.js
@@ -0,0 +1,39 @@
+class Chatroom {
+  constructor() {
+    this.connections = {};
+    this.messages = [];
+  }
+
+  addConnection(connection) {
+    this.connections[connection.nickname] = connection;
+
+    connection.socket.write(
+      this.messages
+        .slice(-10)
+        .map(message => message.format())
+        .join('\n') + '\n'
+    );
+
+    this.broadcast(`${connection.nickname} has entered the chat!`);
+  }
+
+  addMessage(message) {
+    this.messages.push(message);
+
+    this.broadcast(message.format());
+  }
+
+  hasNickname(nickname) {
+    return !!this.connections[nickname];
+  }
+
+  broadcast(text) {
+    Object.keys(this.connections).forEach(key => {
+      const connection = this.connections[key];
+
+      connection.socket.write(text + '\n');
+    });
+  }
+}
+
+module.exports = Chatroom;
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const net = require('net');
+
 const Message = require('./Message');
 const Connection = require('./Connection');
+const Chatroom = require('./Chatroom');
 
-const connections = {};
-
-const messages = [];
+const chatroom = new Chatroom();
 
 const server = net
   .createServer(socket => {
-    console.log('server created');
+    console.log('connection established');
 
     socket.write('Welcome to my chat server! What is your nickname?\n');
 
@@ -19,40 +19,23 @@ const server = net
 
       if (!connection.nickname) {
         // Check if nickname is taken
-        const nicknameIsTaken = connections[response];
+        const nicknameIsTaken = chatroom.hasNickname(response);
 
         if (nicknameIsTaken) {
           socket.write(
-            'That nickname is taken. Please choose another nickname.\n'
+            'That nickname is taken. Please choose anotherz nickname.\n'
           );
         } else {
           // User joins server
           connection.nickname = response;
 
-          connections[response] = connection;
-
-          console.log(JSON.stringify(connections, null, 2));
-
-          socket.write(
-            messages
-              .slice(-10)
-              .map(message => message.format())
-              .join('\n') + '\n'
-          );
-
-          Object.keys(connections).forEach(name => {
-            const conn = connections[name];
-
-            conn.socket.write(`${connection.nickname} has entered the chat!\n`);
-          });
+          chatroom.addConnection(connection);
         }
       } else {
         // User writes message
-        messages.push(
+        chatroom.addMessage(
           new Message({ user: connection.nickname, text: response })
         );
-
-        console.log({ messages });
       }
     });
   })
